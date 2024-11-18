9c87b6f6dad932409f11773068b112e00bfa8e7f
bryndalski <kubabryndal@gmail.com>
2024-11-18 20:43:53 +0100

    Initial commit

---
+++ test/endeavorai.com.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EndeavoraiCom from '../lib/endeavorai.com-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/endeavorai.com-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EndeavoraiCom.EndeavoraiComStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
